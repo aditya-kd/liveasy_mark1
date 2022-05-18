af4851a154a6b16a310a748886a9b10a6a16217d
Aditya Kumar Sharma <adityakd171@gmail.com>
2022-05-18 09:13:35 +0530

    refreshed after PC reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'newcode/authservice.dart';
+
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(  MyApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatelessWidget{
-  MyApp ({Key? key}): super(key: key);
+  const MyApp ({Key? key}): super(key: key);
   @override
   Widget build(BuildContext context){
     return MaterialApp(
